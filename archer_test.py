80ca591aa0b94dcfdde51e76de1e87b2885165a8
DirtyTechSmith <dagontu@gmail.com>
2019-08-12 23:39:04 -0700

    more updates

--- archer_test.py
+++ archer_test.py
@@ -1,4 +1,5 @@
-import sys, pygame
+import sys
+import pygame
 
 pygame.init()
 draw = pygame.draw
@@ -6,19 +7,10 @@ draw = pygame.draw
 size = [1024, 768]
 speed = [1, 1]
 black = 0, 0, 0
-space_bar = pygame.K_SPACE
-keys = {
-    'space' :pygame.K_SPACE,
-}
-print('spacebar')
-print(space_bar)
+
 screen = pygame.display.set_mode(size)
-# image_path = "D:\\proceduworld\\prettyrandomoceansgif.gif"
-# pygame_image = pygame.image.load(image_path)
-# image_rect = pygame_image.get_rect()
 
 circle_color = pygame.color.Color('red')
-
 
 has_focus = 0
 last_pressed_keys = pygame.key.get_pressed()
@@ -36,7 +28,7 @@ while 1:
     if pressed_keys != last_pressed_keys:
         # print('keys changed')
         last_pressed_keys = pressed_keys
-        if pressed_keys[space_bar]:
+        if pressed_keys[pygame.K_SPACE]:
             print('spacetime')
 
     my_cool_new_circle = pygame.draw.circle(screen, circle_color, (100, 100), 100)
