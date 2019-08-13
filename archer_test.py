427c2a9667d7f14017c863b29bef515a13f304db
DirtyTechSmith <dagontu@gmail.com>
2019-08-12 23:42:33 -0700

    updates

--- archer_test.py
+++ archer_test.py
@@ -9,13 +9,19 @@ speed = [1, 1]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+the_surface = pygame.display.get_surface()
 
-circle_color = pygame.color.Color('red')
+bad_guy_color = pygame.color.Color('red')
+archer_color = pygame.color.Color('green')
 
 has_focus = 0
 last_pressed_keys = pygame.key.get_pressed()
+screen.fill(black)
+radius = 50
+good_guy_pos = [radius, size[1] - radius]
+bad_guy_pos = [size[0] - radius, size[1] - radius]
 while 1:
-    screen.fill(black)
+
     focus = pygame.key.get_focused()
     if has_focus != focus:
         print(f'focused changed to: {focus}')
@@ -31,6 +37,7 @@ while 1:
         if pressed_keys[pygame.K_SPACE]:
             print('spacetime')
 
-    my_cool_new_circle = pygame.draw.circle(screen, circle_color, (100, 100), 100)
+    archer_circle = pygame.draw.circle(screen, archer_color, good_guy_pos, radius)
+    bad_guy_circle = pygame.draw.circle(screen, bad_guy_color, bad_guy_pos, radius)
     # screen.blit(my_cool_new_circle)
     pygame.display.flip()
--- classes/archer.py
+++ classes/archer.py
@@ -1,5 +1,109 @@
-class Archer(object):
+import tensorflow as tf
+import pygame
+
+MAX_BOW_STR = 100.0
+
+
+class ArcherBrain(tf.keras.Model):
     def __init__(self):
+        super().__init__()
+        self.dense1 = tf.keras.layers.Dense(5, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(3, activation=tf.nn.softmax)
+
+    def call(self, inputs):
         """
+
+        Args:
+            inputs (list[float]):
+
+        Returns:
+            list[float]
+        """
+        x = self.dense1(inputs)
+        return self.dense2(x)
+
+
+class Archer(object):
+    def __init__(self, position, screen, screen_size, bow_str=100.0, brain=None):
+        """
+
+        Args:
+            position (list[float]):
+            screen():
+            screen_size(list[int]):
+            bow_str ():
+            brain(ArcherBrain):
+        """
+        self._bow_str = bow_str
+        self._brain = brain
+        self._position = position
+        self._screen_size = screen_size
+
+    @property
+    def position(self):
+        """
+
+        Returns:
+            list[int]:
+        """
+        return self._position
+
+    @property
+    def screen_size(self):
+        """
+
+        Returns:
+            list[int]:
+        """
+        return self._screen_size
+
+    @property
+    def relative_position(self):
+        """
+
+        Returns:
+            list[float]:
+        """
+        relative_pos = [float(self.position[0] / self.screen_size[0]), float(self.position[1] / self.screen_size[1])]
+
+    @property
+    def brain(self):
+        """
+
+        Returns:
+            ArcherBrain:
+        """
+        if self._brain is None:
+            self._brain = self.createBrain()
+
+        return self._brain
+
+    @staticmethod
+    def createBrain():
+        """
+
+        Returns:
+            ArcherBrain:
+        """
+        the_brain = ArcherBrain()
+        return the_brain
+
+    def calculate_relative_pos(self, pos):
+        """
+
+        Args:
+            pos (list[int]):
+
+        Returns:
+            list[float]:
+        """
+
+    def shoot(self, enemy_position):
+        """
+
+        Args:
+            enemy_position ():
+
+        Returns:
 
         """
