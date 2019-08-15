162662b7703e036a6f5aa7ba88fa1761caf744fe
Chris Cole <ccole@blizzard.com>
2019-08-14 18:52:39 -0700

    Update bow.py

--- classes/bow.py
+++ classes/bow.py
@@ -1,5 +1,32 @@
 from .arrow import Arrow
 
+lbs_to_newtons = 4.4482216282509
+
 
 class Bow(object):
-    def __init__(self, draw_weight, efficiency=0.9):
+    def __init__(self, draw_weight_lbs=100.0, draw_length_in=30.0, efficiency=0.9):
+        """
+        
+        Args:
+            draw_weight_lbs (float):
+            efficiency ():
+        """
+        self._draw_weight_lbs = draw_weight_lbs
+    
+    @property
+    def draw_weight_lbs(self):
+        """
+        
+        Returns:
+            float:
+        """
+        return self._draw_weight_lbs
+    
+    @property
+    def newton_force(self):
+        """
+        
+        Returns:
+            float:
+        """
+        newtons = self.
