19955622d68415044e3c3a1969346a8927166da6
DirtyTechSmith <dagontu@gmail.com>
2019-11-09 01:55:05 -0800

    Update archer_population.py
    got mutation working.

--- classes/archer_population.py
+++ classes/archer_population.py
@@ -2,6 +2,10 @@ from .archer import Archer
 from .entity import Entity
 from pygame import Surface
 from vector import Vector
+import random
+import tensorflow as tf
+
+Dense = tf.keras.layers.Dense
 
 
 class ArcherPopulation(object):
@@ -59,17 +63,56 @@ class ArcherPopulation(object):
             archer.volley_Threaded()
             archer.waitForFire()
 
+    def mutateLayer(self, layer, mutation_rate=1.5):
+        """
+
+        Args:
+            layer (Dense):
+            mutation_rate(float):
+        Returns:
+            Dense:
+        """
+        for index, value in enumerate(layer.weights):
+
+            mutation_check = random.uniform(0.0, 100.0)
+            if mutation_check > mutation_rate:
+                continue
+
+            print('IMA MUTIN BRO')
+            mutate_amount = random.uniform(-0.1, 0.1)
+            value.assign(value + mutate_amount)
+            # if value > 1.0:
+            #     value = 1.0
+            #
+            # elif value < -1.0:
+            #     value = -1.0
+
+            layer.weights[index] = value
+
+        mutation_check = random.uniform(0.0, 100.0)
+        if mutation_check <= mutation_rate:
+            mutate_amount = random.uniform(-0.1, 0.1)
+            layer.bias.assign(layer.bias + mutate_amount)
+
+        return layer
+
     def breedPopulation(self):
         the_misses = [archer.miss for archer in self.archers.values()]
         biggest_miss = max(the_misses)
         closest_hit = min(the_misses)
-        print(f'biggest miss: {biggest_miss}, closest hit: {closest_hit}')
+        # print(f'biggest miss: {biggest_miss}, closest hit: {closest_hit}')
+        # lets determine our fitness
         for archer in self.archers.values():
-            if miss == 0:
+            if archer.miss == 0:
                 archer.fitness = 1.0
                 print('WE GOT A STRONK BOI')
                 continue
 
-            normalized_miss = miss / biggest_miss
+            normalized_miss = archer.miss / biggest_miss
             fitness = 1.0 - normalized_miss
             archer.fitness = fitness
+
+        # we determined fitness and breed, now for mutation
+        for archer in self.archers.values():
+            archer.brain.hidden_1 = self.mutateLayer(archer.brain.hidden_1)
+            archer.brain.hidden_2 = self.mutateLayer(archer.brain.hidden_2)
